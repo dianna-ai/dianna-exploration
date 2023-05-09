25492550163d19f42fbe097198144425b3af0a01
WillemSpek <willemebe.vd.spek@gmail.com>
2023-05-09 17:05:30 +0200

    Added suggested changes

--- relevance_maps_properties/metrics/metrics.py
+++ relevance_maps_properties/metrics/metrics.py
@@ -6,9 +6,11 @@ from dianna import utils
 from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import auc
-
+from scipy.stats import mode
 
 import matplotlib.pyplot as plt
+
+np.random.seed(0)
 
 
 class Incremental_deletion():
@@ -48,7 +50,7 @@ class Incremental_deletion():
         Returns: 
             The model scores for each iteration of deleted pixels.
         '''
-        impute = self._make_impute(input_img, impute_method)
+        impute_value = self._make_impute(input_img, impute_method)
         salient_order = self._get_salient_order(salience_map)
         eval_img = np.copy(input_img) # Perform deletion on a copy
 
@@ -63,7 +65,7 @@ class Incremental_deletion():
             for k in tqdm(range(n_iters), desc='Evaluating'):
                 # Create batch and score model 
                 partial_order = salient_order[k * self.step * batch_size: (k + 1) * self.step * batch_size]
-                batch = self._create_batch(eval_img, partial_order, impute, batch_size)
+                batch = self._create_batch(eval_img, partial_order, impute_value, batch_size)
                 score = self.model(batch, **model_kwargs)[:, gold_lbl]
                 scores[k * batch_size:(k + 1) * batch_size] = score
         # Handle models without batching
@@ -76,7 +78,7 @@ class Incremental_deletion():
             for k in tqdm(range(n_iters), desc='Evaluating'):
                 # Delete pixels and score model
                 i, j = zip(*salient_order[k * self.step: (k + 1) * self.step]) 
-                eval_img[i, j] = impute
+                eval_img[i, j] = impute_value
                 score = np.max(self.model(eval_img, **model_kwargs), axis=0)[gold_lbl]
                 scores[k + 1] = score
         return scores
@@ -93,13 +95,13 @@ class Incremental_deletion():
                                          salience_map.shape), axis=-1)[::-1]
     
     def _create_batch(self, eval_img: NDArray, salient_order: NDArray, 
-                      impute: Union[NDArray, float], batch_size: int):
+                      impute_value: Union[NDArray, float], batch_size: int) -> NDArray:
         '''Create a batch that can be used by the batched_evaluate method. 
         
         Args: 
             eval_img: Working image to delete pixels on.
             salient_order: Order to delete pixels.
-            impute: Value to replace to be deleted pixels with.
+            impute_value: Value to replace to be deleted pixels with.
             batch_size: Size of batch for model inference.
         Raises: 
             AssertionError: If the `salient order` shape does not hold enough values 
@@ -113,7 +115,7 @@ class Incremental_deletion():
         for k in range(batch_size):
             # Delete pixels and add to batch
             i, j = zip(*salient_order[k * self.step: (k + 1) * self.step]) 
-            eval_img[i, j] = impute
+            eval_img[i, j] = impute_value
             batch[k] = eval_img
         return batch
 
@@ -144,6 +146,8 @@ class Incremental_deletion():
                 return np.median(input_img, axis=(0,1))
             elif impute_method == 'full_median':
                 return float(np.median(input_img, axis=None))
+            elif impute_method == 'mode':
+                return np.m
             else:
                 raise ValueError( f"Given method {impute_method} is not supported. Please "
                                     "choose from 'channel_mean', 'full_mean',"
@@ -153,6 +157,27 @@ class Incremental_deletion():
                 raise ValueError(f"Shape mismatch for given impute_method value {impute_method}."
                                   "Shape must have dimension (..., 3)")
         return impute_method
+    
+    # def visualize_examples(self,
+    #                        salience_map: NDArray,
+    #                        input_img: NDArray,
+    #                        n_examples: int = 5,
+    #                        impute_method: Union[NDArray, float, str] = 'channel_mean',
+    #                        save_to: Optional[str] = None, 
+    #                        **kwargs) -> None:
+    #     imgs, n_removed = np.empty(n_examples)
+
+    #     eval_img = np.copy(input_img)
+    #     for k in range(n_examples):
+    #         for i, j in self._get_salient_order(salience_map):
+    #             eval_img[i, j, ...] = impute_value
+
+    #         score = np.max(self.model(z[None, ...]), axis=0)[gold_lbl]
+    #         preds.append(pred)
+    #         imgs.append(to_img(z))
+    #         removed.append(k)
+
+    #     return imgs, preds, removed
 
     @staticmethod
     def visualize(scores: NDArray, save_to: Optional[str] = None, **kwargs) -> None:
@@ -183,9 +208,20 @@ class Incremental_deletion():
             plt.show()
 
 
-def single_deletion():
-    pass
+class Single_deletion():
+    def __init__(self, model: Union[Callable, str], 
+                 tokenizer: Union[Callable, str], 
+                 preprocess_function: Optional[Callable] = None) -> None:
+        utils.get_function(model, preprocess_function)  
+        pass
+    
+    def evaluate(self, input_string,):
+        pass
 
+    def _get_salient_order(self, salience_map: list[tuple]) -> tuple[NDArray, NDArray]:
+        ordered = sorted(salience_map, key= lambda x: x[-1], reverse=True)
+        _, salient_order, relevance = zip(*ordered)
+        return np.array(salient_order), np.array(relevance)
 
 def fidelity_check(model: Callable, salience_map: NDArray, x: NDArray):
     '''
@@ -205,12 +241,19 @@ def fidelity(model: Callable, expl: NDArray, X: NDArray, cutoff: float = 0.05) -
     pass
 
 
-def stability(model: Callable, x: NDArray, n_samples: int = 10) -> float:
+def stability(explainer: Union[Callable, str], 
+              instance: NDArray, 
+              metric: Callable, 
+              n_calls: int = 10) -> float:
     '''
     Compute the stability of the model for instance x by computing the variance 
-    of n_samples queries to the model.
+    of n_samples queries to the model.      
     '''
-    pass
+    salience_maps = np.empty(shape=(n_calls, *instance.shape))
+    for i in range(n_calls):
+        salience_maps[i] = explainer(instance)
+
+    return metric(salience_maps)
 
 
 def robustness(model: Callable, x: NDArray, n_samples: int = 10) -> float:
