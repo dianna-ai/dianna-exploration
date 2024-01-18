9c661eb3ef01a913691781330176ec439a042296
Qi <q.huang@liacs.leidenuniv.nl>
2024-01-18 11:18:54 +0100

    GradCam Roundedness

--- lorentz_workshop/gradcam/cam.py
+++ lorentz_workshop/gradcam/cam.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 import pickle
 from sklearn.preprocessing import StandardScaler
 import argparse
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget, BinaryClassifierOutputTarget
 
 class ShapesNet(nn.Module):
     def __init__(self, kernels=[8, 16], dropout = 0.2, classes=2):
@@ -90,9 +91,14 @@ def get_loader(data_path,batch_size):
     # data                    = pickle.load(open(data_path, 'rb'))
     # data = np.load("../../example_data/dataset_preparation/geometric_shapes/test_colors.npz")
     data = dict(np.load(data_path))
-    data['images']          = np.array(data['images']) / 255.
+    if 'color' in data_path.split('/')[-1]:
+        data['images']          = np.array(data['images']) / 255.
+    elif 'rotation' in data_path.split('/')[-1]:
+        data['images']          = np.array(data['images']) / 255.
+    elif 'roundedness' in data_path.split('/')[-1]:
+        data['images']          = np.array(data['images'])
     data['images']          = data['images'][:100]
-    data['labels']          = np.array(data['labels']) / 255.
+    data['labels']          = np.array(data['labels'])
     data['labels']          = data['labels'][:100]
     test_X_torch = torch.from_numpy(data['images']).type(torch.FloatTensor)
     test_y_torch = torch.from_numpy(data['labels']).type(torch.LongTensor)
@@ -160,7 +166,7 @@ if __name__ == '__main__':
     imgs                = data['images']
     model               = ShapesNet().to('cpu')
     print(model.layer2)
-    criterion           = nn.CrossEntropyLoss().to('cpu')
+    # criterion           = nn.CrossEntropyLoss().to('cpu')
     model.load_state_dict(torch.load(os.path.join(MODEL_PATH))['model_state_dict'])
 
     correct             = 0
@@ -169,14 +175,14 @@ if __name__ == '__main__':
     for batch_idx, (test_imgs, test_labels) in enumerate(test_loader):
         test_imgs       = torch.autograd.Variable(test_imgs).float()
         cam             = GradCAM(model=model, target_layers=model.layer2)
-        grayscale_cam   = cam(input_tensor=test_imgs, targets=None)
+        grayscale_cam   = cam(input_tensor=test_imgs, targets=[ClassifierOutputTarget(torch.argmax(model(test_imgs)))])
 
         # In this example grayscale_cam has only one image in the batch:
         grayscale_cam   = grayscale_cam[0, :]
-        grayscale_cam   = (grayscale_cam - grayscale_cam.min())/(grayscale_cam.max() - grayscale_cam.min())
-        rgb_img         = (np.array(test_imgs[0,0])*255).astype('uint8')
-        rgb_img         = cv2.cvtColor(rgb_img, cv2.COLOR_GRAY2RGB)
-        rgb_img         = rgb_img/255.
+        grayscale_cam   = (grayscale_cam - grayscale_cam.min())/(grayscale_cam.max() - grayscale_cam.min() + 10e-7)
+        # rgb_img         = (np.array(test_imgs[0,0])*255).astype('uint8')
+        # rgb_img         = cv2.cvtColor(rgb_img, cv2.COLOR_GRAY2RGB)
+        # rgb_img         = rgb_img/255.
 
         heatmap         = cv2.applyColorMap(np.uint8(255 * grayscale_cam), cv2.COLORMAP_TWILIGHT_SHIFTED)
 
